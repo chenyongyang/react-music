d5118762420123bd87a69f2108e358aaa980e89e
ChenYongYang <1214137301@qq.com>
2018-10-21 20:28:03 +0800

    '添加loading组件'

---
+++ src/base/loading/loading.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './loading.scss'
+const Loading = (props) => {
+    const { text } = props;
+    return (
+        <div className="loading-box">
+            <div className="loading">
+                <div>
+                    <span className="odd" />
+                    <span className="double" />
+                    <span className="odd" />
+                    <span className="double" />
+                </div>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+};
+Loading.defaultProps = {
+    text: '正在加载中...'
+};
+export default Loading 
